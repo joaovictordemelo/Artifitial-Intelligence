1fc979374594391391bb6411ab810dbf587b06d3
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-08-09 11:31:10 -0300

    Update DatabaseSQL.sql

--- DatabaseSQL.sql
+++ DatabaseSQL.sql
@@ -1,4 +1,4 @@
-//practice 
+--practice 
 
 SELECT 
 	date,
@@ -17,7 +17,7 @@ WHERE (awayteam_id = 8634 OR hometeam_id = 8634)
       AND (awayteam_id = 8633 OR hometeam_id = 8633);
       
       
-//link: https://campus.datacamp.com/courses/intermediate-sql/well-take-the-case?ex=6
+--link: https://campus.datacamp.com/courses/intermediate-sql/well-take-the-case?ex=6
 
 
 
@@ -53,4 +53,46 @@ WHERE
     
     
     
+    
+ SELECT 
+	c.name AS country,
+    -- Count games from the 2012/2013 season
+	COUNT(CASE WHEN m.season = '2012/2013' 
+          	   THEN m.id ELSE NULL END) AS matches_2012_2013
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+
+SELECT 
+	c.name AS country,
+    -- Count matches in each of the 3 seasons
+	COUNT(CASE WHEN m.season = '2012/2013' THEN m.id ELSE NULL END) AS matches_2012_2013,
+	COUNT(CASE WHEN m.season = '2013/2014' THEN m.id ELSE NULL END) AS matches_2013_2014,
+	COUNT(CASE WHEN m.season = '2014/2015' THEN m.id ELSE NULL END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
+
+SELECT 
+	c.name AS country,
+    -- Sum the total records in each season where the home team won
+	SUM(CASE WHEN m.season = '2012/2013' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2012_2013,
+	SUM(CASE WHEN m.season = '2013/2014' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2013_2014,
+	SUM(CASE WHEN m.season = '2014/2015' AND m.home_goal > m.away_goal 
+        THEN 1 ELSE 0 END) AS matches_2014_2015
+FROM country AS c
+LEFT JOIN match AS m
+ON c.id = m.country_id
+-- Group by country name alias
+GROUP BY country;
+
     
