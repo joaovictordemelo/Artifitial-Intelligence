9e48446e03bb17824ad0eeb2c00552ac07b1fa7b
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-08-12 17:31:28 -0300

    Update DatabaseSQL.sql

--- DatabaseSQL.sql
+++ DatabaseSQL.sql
@@ -178,3 +178,26 @@ INNER JOIN (SELECT country_id, id
 ON c.id = sub.country_id
 GROUP BY country_name;
 
+
+
+--Calculate the average home goals and average away goals from the match table for each stage in the FROM clause subquery. 
+--Add a subquery to the WHERE clause that calculates the overall average home goals.
+--Filter the main query for stages where the average home goals is higher than the overall average.
+--Select the stage and avg_goals columns from the s subquery into the main query.
+
+SELECT 
+	-- Select the stage and average goals from the subquery
+	s.stage,
+	ROUND(s.avg_goals,2) AS avg_goals
+FROM 
+	-- Select the stage and average goals in 2012/2013
+	(SELECT
+		 stage,
+         AVG(home_goal + away_goal) AS avg_goals
+	 FROM match
+	 WHERE season = '2012/2013'
+	 GROUP BY stage) AS s
+WHERE 
+	-- Filter the main query using the subquery
+	s.avg_goals > (SELECT AVG(home_goal + away_goal) 
+                    FROM match WHERE season = '2012/2013');
