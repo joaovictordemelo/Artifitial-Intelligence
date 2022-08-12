39cc0b15fbb2faf39e2693b42799d486c0a655af
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-08-12 16:42:00 -0300

    Update DatabaseSQL.sql

--- DatabaseSQL.sql
+++ DatabaseSQL.sql
@@ -126,3 +126,16 @@ FROM country AS c
 LEFT JOIN matches AS m
 ON c.id = m.country_id
 GROUP BY country;
+
+
+
+--Select the team_long_name and team_short_name from the team table. Exclude all values from the subquery in the main query.
+
+SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN
+     (SELECT DISTINCT hometeam_id FROM match);
