7f9e1a6d6c641f39ece044a9776e56033a981260
joaovictordemelo <69903635+joaovictordemelo@users.noreply.github.com>
2022-08-17 23:53:08 -0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,167 @@
+#list of common stuff https://datacamp-community-prod.s3.amazonaws.com/0eff0330-e87d-4c34-88d5-73e80cb955f2
+
+
+#log x axis
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+
+plt.xscale('log')
+
+# Show plot
+plt.show()
+
+
+
+#histograms
+
+# Create histogram of life_exp data
+
+
+
+plt.hist(life_exp, bins=10)
+
+# Display histogram
+plt.show()
+
+
+
+
+
+#clean and plot new histogram
+
+
+# Build histogram with 5 bins
+
+plt.hist(life_exp, bins = 5)
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+
+plt.hist(life_exp, bins = 20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+
+
+#add x, y and title
+
+# Basic scatter plot, log scale
+plt.scatter(gdp_cap, life_exp)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+
+
+# Add title
+
+plt.title(title)
+# After customizing, display the plot
+plt.show()
+
+
+
+
+
+
+#using ticks for changing the axis
+
+
+# Scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks(tick_val, tick_lab)
+
+# After customizing, display the plot
+plt.show()
+
+
+
+
+
+#scatter with point size (where the size of the dots correspond to the size of the population)
+
+# Import numpy as np
+
+import numpy as np
+
+# Store pop as a numpy array: np_pop
+
+np_pop = np.array(pop)
+
+# Double np_pop
+np_pop = 2*np_pop
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = np_pop)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
+plt.show()
+
+
+
+
+#getting indexes from a list and printing
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index("germany")
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
+
+
+
+#relation between keys and values
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+europe.keys()
+
+# Print out value that belongs to key 'norway'
+europe['norway']
+#remove item from dictionary
+del(europe['australia'])
+
+
+
+
+
